df1bf27d2a4ef378ef99bdcc8695e2729d3d576c
33arsenicum <cmykbielaruski@gmail.com>
2023-08-06 17:00:20 +0300

    feat: the task is solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  const numString = n.toString();
+  const digitsArray = numString.split('');
+  digitsArray.reverse();
+  const reversedNumString = digitsArray.join('');
+  const reversedNumber = parseInt(reversedNumString);
+  return reversedNumber;
 }
